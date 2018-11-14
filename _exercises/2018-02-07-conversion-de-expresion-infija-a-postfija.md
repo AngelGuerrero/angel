81b3299d03c462d4fe8ab3625b6b57ff9ab16da0
Ángel Guerrero <lasantiagoguerrero@gmail.com>
2018-11-14 04:03:40 -0600

    Nuevo post, sobre java y el algoritmo para convertir expresiones infijas a postfijas

---
+++ _exercises/2018-02-07-conversion-de-expresion-infija-a-postfija.md
@@ -0,0 +1,75 @@
+---
+title: Conversion de expresion infija a postfija
+date: 2018-02-07 18:40:49 +0000
+curr_img_path: /postfijo
+image: assets/img/java/postfijo/portada.PNG
+img_dir: /assets/img/java
+repo: https://github.com/AngelGuerrero/infijo-postfijo-java
+published: true
+---
+Como ejercicio escolar me han dejado que desarrolle la evaluación de una expresión infija y convertirla a postfija
+específicamente en Java.
+
+<a href="{{ site.baseurl }}{{ page.img_dir }}{{ page.curr_img_path }}/aplicacion.PNG">
+  <img src="{{ site.baseurl }}{{ page.img_dir }}{{ page.curr_img_path }}/aplicacion.PNG" alt="">
+</a>
+
+
+# ¿Qué es una expresión infija?
+
+Esta es una notación común de fórmulas aritméticas y lógicas, la notación postfija es una evaluación de expresiones
+aritméticas mediante pilas.
+
+De acuerdo a las operaciones que se necesitan implementar no se utilizan del todo "pilas", mi solución fue utilizar dos
+pilas para mostrar el resultado, y dos listas enlazadas simples.
+
+<a href="{{ site.baseurl }}{{ page.img_dir }}{{ page.curr_img_path }}/variables.PNG">
+  <img src="{{ site.baseurl }}{{ page.img_dir }}{{ page.curr_img_path }}/variables.PNG" alt="Variables" class="img__responsive">
+</a>
+
+En la imagen de arriba se puede observar que he declarado una pila para obtner el resultado final, y una lista
+doblemente enlazada para listar los operadores que pudiera tener la expresión.
+
+Además de definir las letras con las que se puede trabajar, sus operadores y la definición de separadores de apertura y
+cierre, así se tiene un mejor control de qué se está evaluando.
+
+<a href="{{ site.baseurl }}{{ page.img_dir }}{{ page.curr_img_path }}/variables_2.PNG">
+  <img src="{{ site.baseurl }}{{ page.img_dir }}{{ page.curr_img_path }}/variables_2.PNG" alt="" srcset="">
+</a>
+
+Antes de comenzar a realizar el procedimiento es necesario definir la prioridad de los operadores existentes, en mi
+caso lo he dejado de la siguiente manera:
+
+<a href="{{ site.baseurl }}{{ page.img_dir }}{{ page.curr_img_path }}/prioridad_de_operadores.PNG">
+  <img src="{{ site.baseurl }}{{ page.img_dir }}{{ page.curr_img_path }}/prioridad_de_operadores.PNG" alt="" srcset="">
+</a>
+
+A la función anterior que le he llamado `compare` se le pasa como parámetro un carácter y ésta decide cuál es su valor
+de acuerdo a la lógica que le he establecido.
+
+Se inicia recorriendo la expresión carácter por carácter y de acuerdo a este carácter se realiza una serie de
+condicionales para saber cuál es la que cumple dicho carácter.
+
+* Si el carácter es una letra o una literal, se agrega directamente a la pila de salida, `stackOutput.`
+* Si no, si el carácter es un separador de apertura o más bien, éste está dentro de la lista de caracteres que he
+definido, entonces, se agrega también directamente a la pila final `stackOutput`, esto lo hago con el fin de tener un
+"comodín" y saber dónde comienza o cierra ciertos operadores de la expresión.
+* Si no, entonces el caracter es un operador, aquí se realiza unas condicionales anidadas.
+* Verifiaca si la lista está vacía, si no es así, agrega el operador a la lista de operadores, cabe decir que si el
+operador es un operador de apertura tamibén se agrega a la lista, sólo que al momento de salir estos simplemente se
+ignoran, como dije se tienen únicamente de referencia.
+* Si la lista no está vacía pues entonces comienza a realizar comparaciones. Aquí es donde viene la parte clave del
+algoritmo.
+* La regla dice que si los operadores de la lista tienen mayor prioridad o misma prioridad que el operador que se está
+evaluando actualmente, entonces todos estos se sacan de la lista a menos que se encuentren un operador de apertura en
+la lista, y al final se agrega el operador que actualmente se está evaluando a la lista. Para estas operaciones utilicé
+una lista auxiliar para poder mover los elementos de la lista y realizar el recorrido de la misma, como si fuese una
+torre de Hanoi.
+* Por último, si el ciclo llega al final entonces se vuelca toda la lista de los operadores en la pila donde se
+almacena el resultado final y simplemente se muestra de la forma correcta, yo utilicé una pila más como pila auxiliar
+para mostar los datos correctamente.
+
+<a href="{{ site.baseurl }}{{ page.img_dir }}{{ page.curr_img_path }}/portada.PNG">
+  <img class="img__responsive" src="{{ site.baseurl }}{{ page.img_dir }}{{ page.curr_img_path }}/portada.PNG" alt=""
+    srcset="">
+</a>
---
+++ dist/main.be008001a02b9fcadf8e.js
@@ -0,0 +1,229 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// install a JSONP callback for chunk loading
+/******/ 	function webpackJsonpCallback(data) {
+/******/ 		var chunkIds = data[0];
+/******/ 		var moreModules = data[1];
+/******/ 		var executeModules = data[2];
+/******/
+/******/ 		// add "moreModules" to the modules object,
+/******/ 		// then flag all "chunkIds" as loaded and fire callback
+/******/ 		var moduleId, chunkId, i = 0, resolves = [];
+/******/ 		for(;i < chunkIds.length; i++) {
+/******/ 			chunkId = chunkIds[i];
+/******/ 			if(installedChunks[chunkId]) {
+/******/ 				resolves.push(installedChunks[chunkId][0]);
+/******/ 			}
+/******/ 			installedChunks[chunkId] = 0;
+/******/ 		}
+/******/ 		for(moduleId in moreModules) {
+/******/ 			if(Object.prototype.hasOwnProperty.call(moreModules, moduleId)) {
+/******/ 				modules[moduleId] = moreModules[moduleId];
+/******/ 			}
+/******/ 		}
+/******/ 		if(parentJsonpFunction) parentJsonpFunction(data);
+/******/
+/******/ 		while(resolves.length) {
+/******/ 			resolves.shift()();
+/******/ 		}
+/******/
+/******/ 		// add entry modules from loaded chunk to deferred list
+/******/ 		deferredModules.push.apply(deferredModules, executeModules || []);
+/******/
+/******/ 		// run deferred modules when all chunks ready
+/******/ 		return checkDeferredModules();
+/******/ 	};
+/******/ 	function checkDeferredModules() {
+/******/ 		var result;
+/******/ 		for(var i = 0; i < deferredModules.length; i++) {
+/******/ 			var deferredModule = deferredModules[i];
+/******/ 			var fulfilled = true;
+/******/ 			for(var j = 1; j < deferredModule.length; j++) {
+/******/ 				var depId = deferredModule[j];
+/******/ 				if(installedChunks[depId] !== 0) fulfilled = false;
+/******/ 			}
+/******/ 			if(fulfilled) {
+/******/ 				deferredModules.splice(i--, 1);
+/******/ 				result = __webpack_require__(__webpack_require__.s = deferredModule[0]);
+/******/ 			}
+/******/ 		}
+/******/ 		return result;
+/******/ 	}
+/******/
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// object to store loaded and loading chunks
+/******/ 	// undefined = chunk not loaded, null = chunk preloaded/prefetched
+/******/ 	// Promise = chunk loading, 0 = chunk loaded
+/******/ 	var installedChunks = {
+/******/ 		"main": 0
+/******/ 	};
+/******/
+/******/ 	var deferredModules = [];
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/angel/dist/";
+/******/
+/******/ 	var jsonpArray = window["webpackJsonp"] = window["webpackJsonp"] || [];
+/******/ 	var oldJsonpFunction = jsonpArray.push.bind(jsonpArray);
+/******/ 	jsonpArray.push = webpackJsonpCallback;
+/******/ 	jsonpArray = jsonpArray.slice();
+/******/ 	for(var i = 0; i < jsonpArray.length; i++) webpackJsonpCallback(jsonpArray[i]);
+/******/ 	var parentJsonpFunction = oldJsonpFunction;
+/******/
+/******/
+/******/ 	// add entry module to deferred list
+/******/ 	deferredModules.push(["./src/main.js","vendor"]);
+/******/ 	// run deferred modules when ready
+/******/ 	return checkDeferredModules();
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./node_modules/babel-loader/lib/index.js!./node_modules/vue-loader/lib/index.js?!./src/components/Cite.vue?vue&type=script&lang=js&":
+/*!********************************************************************************************************************************************!*\
+  !*** ./node_modules/babel-loader/lib!./node_modules/vue-loader/lib??vue-loader-options!./src/components/Cite.vue?vue&type=script&lang=js& ***!
+  \********************************************************************************************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! axios */ \"./node_modules/axios/index.js\");\n/* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(axios__WEBPACK_IMPORTED_MODULE_0__);\n//\n//\n//\n//\n//\n//\n//\n//\n//\n\n\n\n/* harmony default export */ __webpack_exports__[\"default\"] = ({\n  name: 'Cite',\n\n  data: function data() {\n    return {\n      quote: {},\n      date: ''\n    };\n  },\n\n\n  watch: {\n    getQuote: function getQuote() {\n      this.getQuote();\n    }\n  },\n\n  mounted: function mounted() {\n    this.getQuote();\n  },\n\n  methods: {\n    getQuote: function getQuote() {\n      var _this = this;\n\n      axios__WEBPACK_IMPORTED_MODULE_0___default.a.get(\"https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=\").then(function (response) {\n        return _this.quote = response.data[0];\n      }).catch(function (err) {\n        return console.err(err);\n      });\n    }\n  }\n});\n\n//# sourceURL=webpack:///./src/components/Cite.vue?./node_modules/babel-loader/lib!./node_modules/vue-loader/lib??vue-loader-options");
+
+/***/ }),
+
+/***/ "./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./src/components/Cite.vue?vue&type=template&id=ff7a7bf2&":
+/*!**********************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib??vue-loader-options!./src/components/Cite.vue?vue&type=template&id=ff7a7bf2& ***!
+  \**********************************************************************************************************************************************************************************************/
+/*! exports provided: render, staticRenderFns */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"render\", function() { return render; });\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"staticRenderFns\", function() { return staticRenderFns; });\nvar render = function() {\n  var _vm = this\n  var _h = _vm.$createElement\n  var _c = _vm._self._c || _h\n  return _c(\"div\", { staticClass: \"header__cite\" }, [\n    _c(\"i\", [\n      _c(\"blockquote\", {\n        staticClass: \"cite\",\n        attrs: { title: _vm.quote.title, cite: _vm.quote.link },\n        domProps: { innerHTML: _vm._s(_vm.quote.content) }\n      })\n    ]),\n    _vm._v(\" \"),\n    _c(\"div\", { staticClass: \"cite__author\" }, [\n      _c(\"strong\", { domProps: { innerHTML: _vm._s(_vm.quote.title) } })\n    ])\n  ])\n}\nvar staticRenderFns = []\nrender._withStripped = true\n\n\n\n//# sourceURL=webpack:///./src/components/Cite.vue?./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib??vue-loader-options");
+
+/***/ }),
+
+/***/ "./src/components/Cite.vue":
+/*!*********************************!*\
+  !*** ./src/components/Cite.vue ***!
+  \*********************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _Cite_vue_vue_type_template_id_ff7a7bf2___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./Cite.vue?vue&type=template&id=ff7a7bf2& */ \"./src/components/Cite.vue?vue&type=template&id=ff7a7bf2&\");\n/* harmony import */ var _Cite_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./Cite.vue?vue&type=script&lang=js& */ \"./src/components/Cite.vue?vue&type=script&lang=js&\");\n/* empty/unused harmony star reexport *//* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ \"./node_modules/vue-loader/lib/runtime/componentNormalizer.js\");\n\n\n\n\n\n/* normalize component */\n\nvar component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__[\"default\"])(\n  _Cite_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__[\"default\"],\n  _Cite_vue_vue_type_template_id_ff7a7bf2___WEBPACK_IMPORTED_MODULE_0__[\"render\"],\n  _Cite_vue_vue_type_template_id_ff7a7bf2___WEBPACK_IMPORTED_MODULE_0__[\"staticRenderFns\"],\n  false,\n  null,\n  null,\n  null\n  \n)\n\n/* hot reload */\nif (false) { var api; }\ncomponent.options.__file = \"src/components/Cite.vue\"\n/* harmony default export */ __webpack_exports__[\"default\"] = (component.exports);\n\n//# sourceURL=webpack:///./src/components/Cite.vue?");
+
+/***/ }),
+
+/***/ "./src/components/Cite.vue?vue&type=script&lang=js&":
+/*!**********************************************************!*\
+  !*** ./src/components/Cite.vue?vue&type=script&lang=js& ***!
+  \**********************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _node_modules_babel_loader_lib_index_js_node_modules_vue_loader_lib_index_js_vue_loader_options_Cite_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../node_modules/babel-loader/lib!../../node_modules/vue-loader/lib??vue-loader-options!./Cite.vue?vue&type=script&lang=js& */ \"./node_modules/babel-loader/lib/index.js!./node_modules/vue-loader/lib/index.js?!./src/components/Cite.vue?vue&type=script&lang=js&\");\n/* empty/unused harmony star reexport */ /* harmony default export */ __webpack_exports__[\"default\"] = (_node_modules_babel_loader_lib_index_js_node_modules_vue_loader_lib_index_js_vue_loader_options_Cite_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__[\"default\"]); \n\n//# sourceURL=webpack:///./src/components/Cite.vue?");
+
+/***/ }),
+
+/***/ "./src/components/Cite.vue?vue&type=template&id=ff7a7bf2&":
+/*!****************************************************************!*\
+  !*** ./src/components/Cite.vue?vue&type=template&id=ff7a7bf2& ***!
+  \****************************************************************/
+/*! exports provided: render, staticRenderFns */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_Cite_vue_vue_type_template_id_ff7a7bf2___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!../../node_modules/vue-loader/lib??vue-loader-options!./Cite.vue?vue&type=template&id=ff7a7bf2& */ \"./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./src/components/Cite.vue?vue&type=template&id=ff7a7bf2&\");\n/* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, \"render\", function() { return _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_Cite_vue_vue_type_template_id_ff7a7bf2___WEBPACK_IMPORTED_MODULE_0__[\"render\"]; });\n\n/* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, \"staticRenderFns\", function() { return _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_Cite_vue_vue_type_template_id_ff7a7bf2___WEBPACK_IMPORTED_MODULE_0__[\"staticRenderFns\"]; });\n\n\n\n//# sourceURL=webpack:///./src/components/Cite.vue?");
+
+/***/ }),
+
+/***/ "./src/main.js":
+/*!*********************!*\
+  !*** ./src/main.js ***!
+  \*********************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var vue__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! vue */ \"./node_modules/vue/dist/vue.esm.js\");\n/* harmony import */ var _components_Cite__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./components/Cite */ \"./src/components/Cite.vue\");\n\n\n\n/* eslint-disable no-new */\nnew vue__WEBPACK_IMPORTED_MODULE_0__[\"default\"]({\n  el: '#app',\n\n  components: {\n    externalCite: _components_Cite__WEBPACK_IMPORTED_MODULE_1__[\"default\"]\n  }\n});\n\n//# sourceURL=webpack:///./src/main.js?");
+
+/***/ })
+
+/******/ });
